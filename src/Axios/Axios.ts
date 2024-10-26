42a0e960c4a96700a3b7a68dabce398e23896b28
prateek kushwaha <prateek9574529921@gmail.com>
2024-10-26 09:28:24 +0530

    Add the backend url

--- src/Axios/Axios.ts
+++ src/Axios/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/', 
+  baseURL: 'https://spocifybackend-production.up.railway.app/api/', 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
@@ -10,7 +10,7 @@ export const axiosInstance = axios.create({
 });
 
 export const axiosAuthInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/', 
+  baseURL: 'https://spocifybackend-production.up.railway.app/api/', 
   timeout: 10000, 
   headers: {
     'Content-Type': 'application/json',
